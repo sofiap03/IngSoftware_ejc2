8b8cbae7933606e7f4aebc4d9ace9270a9ad7eff
sofia <sofia.pinuelam@autonoma.edu.co>
2022-03-30 09:04:31 -0500

    End

--- src/services/superhero_v2.service.js
+++ src/services/superhero_v2.service.js
@@ -1,52 +1,58 @@
-const SuperheroRoute = require('../routes/superhero_v2.router')
-const SuperheroModel = require('../models/superhero_v2.model')
+const SuperheroRoute = require('../routes/superhero_v2.router');
+const SuperheroModel = require('../models/superhero_v2.model');
 const Boom = require('@hapi/boom');
 
-class SuperheroService{
+class SuperheroService {
   //Promesas y funciones asincronicas
   //Una funcion asincronica devuelve una promesa
   //Js es un lenguaje ejecuta un hilo -> solo hace una cosa a la vez
-  async createSuperhero(superheroV2){
+  async createSuperhero(superheroV2) {
     superheroV2.save();
     return superheroV2;
   }
 
-  async listSuperhero(){
-    return SuperheroModel.find()
+  async listSuperhero() {
+    return SuperheroModel.find();
   }
 
-//Función que devuelve una promesa
-  find(){
-    return new Promise((resolve)=>{
-      setTimeout(()=>{
+  //Función que devuelve una promesa
+  find() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
         resolve(SuperheroModel.find());
-      }, 3000)
+      }, 3000);
     });
   }
 
-  async showSuperhero(superheroId){
-    return SuperheroModel.findById({ _id: superheroId })
-  }
-
-  async editSuperhero(superheroId, superhero, realname, superpower){
+  async showSuperhero(superheroId) {
     return SuperheroModel.findById({ _id: superheroId }).then(
-      (superheroFind)=> {if(!superheroFind) throw Boom.notFound('No se encontro el superhero');
-      return SuperheroModel.updateOne(
-        { superheroId },
-        { superhero, realname, superpower }
-      );
-    }
+      (superheroFind) => {
+        if (!superheroFind) throw Boom.notFound('No se encontro el superhero');
+        return superheroFind;
+      }
     );
   }
 
-  async removeSuperhero(superheroId){
+  async editSuperhero(superheroId, superhero, realname, superpower) {
     return SuperheroModel.findById({ _id: superheroId }).then(
-      (superheroFind)=> {if(!superheroFind) throw Boom.notFound('No se encontro el superhero');
-      return SuperheroModel.deleteOne(superheroFind);
-    }
+      (superheroFind) => {
+        if (!superheroFind) throw Boom.notFound('No se encontro el superhero');
+        return SuperheroModel.updateOne(
+          { superheroId },
+          { superhero, realname, superpower }
+        );
+      }
     );
   }
 
+  async removeSuperhero(superheroId) {
+    return SuperheroModel.findById({ _id: superheroId }).then(
+      (superheroFind) => {
+        if (!superheroFind) throw Boom.notFound('No se encontro el superhero');
+        return SuperheroModel.deleteOne(superheroFind);
+      }
+    );
+  }
 }
 
 module.exports = SuperheroService;
